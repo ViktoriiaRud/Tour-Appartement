6e653f702f739ad21232ce42b82e9f19045c811c
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-03-13 18:56:32 +0100

    new email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main>
       <div>
-          <h3> new Hello Tour Apartment...</h3>
+          <h3> new Hello Tour Apartment...
+                <p>new</p>
+          </h3>
           <p></p>
       </div>
     </main>
