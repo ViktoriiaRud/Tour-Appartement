42cc6a047ed8104e4ea05c04339851015fdbbe22
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-07-30 13:42:40 +0200

    change name project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import React from 'react';
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import './ui/globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Portfolio",
+    title: "Portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
-}
+const MainLayout = ({children}) => {
+    return (
+        <html lang="en">
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
+};
+
+export default MainLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
-  return (
-    <main>
-      <div>
-          <h3> new Hello Tour Apartment...
-                <p>new</p>
-          </h3>
-          <p></p>
-      </div>
-    </main>
-  );
-}
+const HomePage = () => {
+    return (
+        <div>
+            <h3> new Hello Tour Apartment...</h3>
+        </div>
+    );
+};
+
+export default HomePage;
