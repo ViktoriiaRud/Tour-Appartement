dcb1ce4853cfcb97edb6d1a7578ecbd46423ae85
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-08-01 17:53:13 +0200

    Created layout, homepage and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import {Metadata} from "next";
+import './assets/styles/globals.css'
 
-export default function RootLayout({
+export const metadata: Metadata = {
+    title: "tour-apartment",
+    description: "tour-apartment project - rental apartments",
+    keywords: 'rental, find rentals',
+};
+
+export default function MainLayout({
                                        children,
                                    }: {
     children: React.ReactNode
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 export const metadata = {
-    title: 'apartment'
+    title: 'tour-apartment',
 }
 const HomePage = () => {
     return (
