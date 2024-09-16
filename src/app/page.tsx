60a848fad4e225862935a4bd6434c4442c2b7e54
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-09-16 13:04:14 +0200

    add config database.js(connectDB)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Hero from '@/components/Hero';
 import Boxes from '@/components/Boxes';
 import HomeProperties from '@/components/HomeProperties';
+import connectBD from '@/config/database';
 
 
-const HomePage = () => {
+const HomePage = async () => {
+    await connectBD();
     return (
         <>
            <Hero/>
---
+++ src/config/database.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+let connected = false;
+
+const connectDB = async() => {
+    mongoose.set('strict', true);
+
+    // if database is already connected, don't connect again
+
+
+    if(connected) {
+        console.log('Mongo is already connected');
+        return;
+    }
+
+    // Connect to MongoDB
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+       connected = true;
+       console.log('MongoDB connected')
+
+
+    } catch (error) {
+       console.log('error')
+    }
+};
+
+export default connectDB;
