a6601aef7219b6507ac248262321d2e2dfa6c413
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-09-19 11:13:45 +0200

    bug fix

--- src/app/api/properties/route.tsx
+++ src/app/api/properties/route.tsx
@@ -1,10 +1,10 @@
-// import connectDB from "@/config/database";
+import connectDB from "@/config/database";
 
-// export const GET = async (request: string) => {
-// try {
-//     await connectDB();
-//     return new Response(JSON.stringify({message: "Hello!"}), {status: 200});
-// } catch (error) {
-//     return new Response('Wrong', {status: 500});
-// };
-// }
+export const GET = async (request: string) => {
+try {
+    await connectDB();
+    return new Response(JSON.stringify({message: "Hello!"}), {status: 200});
+} catch (error) {
+    return new Response('Wrong', {status: 500});
+};
+}
